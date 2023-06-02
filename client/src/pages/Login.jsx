067f00d49a9793553720d0e1286211be1bf5f0a4
OppedRawen <yud2372@gmail.com>
2023-06-01 21:02:40 -0700

    browser router, pages, components

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,6 @@
+import React,{useState,useEffect} from 'react';
+
+const Login = () => {
+    <div>Login</div>
+}
+export default Login;
---
+++ client/src/pages/MusicSearchField.jsx
@@ -0,0 +1,6 @@
+import React,{useState,useEffect} from 'react';
+
+const MusicSearch = () => {
+    <div>Search</div>
+}
+export default MusicSearch;
