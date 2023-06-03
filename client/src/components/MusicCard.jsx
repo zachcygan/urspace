81c8a63105f60e12322fb5b65bd45d577d5acef2
OppedRawen <yud2372@gmail.com>
2023-06-03 14:13:12 -0700

    music card text, authors

--- client/src/components/MusicCard.jsx
+++ client/src/components/MusicCard.jsx
@@ -4,11 +4,20 @@ import {useDispatch} from 'react-redux';
 
 
 const MusicCard =({song,i})=>(
-    <div className="flex flex-col w-[250px] px-4 bg-white/5 bg-opacity-100 backdrop-blur-sm animate-slideup rounded-lg curson-pointer" key={i}>
+    <div className="flex flex-col w-[200px] px-4 bg-white/5 bg-opacity-100 backdrop-blur-sm animate-slideup rounded-lg curson-pointer" key={i}>
         <div className='relative w-full h-56 group'>
             <div className={`absolute inset-0 justify-center items-center bg-black bg-opacity-50 group-hover:flex`}>
-                <h1>hello</h1>
+                
             </div>
+            <img src={song.images?.coverart} alt="songImage" className='w-full h-full object-cover rounded-lg'/>
+        </div>
+        <div className='mt-4 flex flex-col'>
+            <p className='font-semibold text-lg  truncate'>
+                <Link to={`/songs/${song?.key}`}>{song.title}</Link>
+            </p>
+            <p className='text-sm truncate  mt-1'>
+                <Link to={song.artists?`/artists/${song?.artists[0].adamid}`:`/top-artists`}>{song.subtitle}</Link>
+            </p>
         </div>
     </div>
 )
