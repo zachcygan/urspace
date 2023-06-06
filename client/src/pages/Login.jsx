a9486cf5ac13cd33a93420ce7dc97a16803ebc5a
chunkybooboo <89316044+chunkybooboo@users.noreply.github.com>
2023-06-06 16:15:10 -0700

    added to login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -5,7 +5,6 @@ const Login = () => {
     return (
         <div class="flex w-screen min-h-full flex-col justify-center px-6 py-12 lg:px-8">
         <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-          <img class="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company"></img>
           <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in to your account</h2>
         </div>
       
