e72cb998d7502728074cbdcf198f062ed8475af7
OppedRawen <yud2372@gmail.com>
2023-06-05 18:59:21 -0700

    env

--- client/src/API/ShazamCore.js
+++ client/src/API/ShazamCore.js
@@ -1,5 +1,7 @@
 
 import {createApi,fetchBaseQuery} from '@reduxjs/toolkit/query/react';
+const dotenv = require('dotenv');
+dotenv.config();
 // const options = {
 //     method: 'GET',
 //     headers: {
@@ -16,7 +18,7 @@ import {createApi,fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 export const shazamCoreApi = createApi({
   reducerPath:'shazamCoreApi',
   baseQuery:fetchBaseQuery({
-    baseUrl:'https://shazam-core.p.rapidapi.com/v1',
+    baseUrl:process.env.baseQuery,
     prepareHeaders:(headers)=>{
       headers.set('X-RapidAPI-Key','d70bb1b6e1mshe04c414a121c6dep153cc2jsn32fdffc7620f');
       return headers;
