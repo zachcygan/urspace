df954fa9519b67c65a92a2ca9b1582c193ffe6c6
zachcygan <zachcygan@gmail.com>
2023-06-07 12:36:45 -0700

    added posts section to profile

--- client/src/components/Posts.jsx
+++ client/src/components/Posts.jsx
@@ -19,18 +19,32 @@ const posts = [
           'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
       },
     },
+    {
+      id: 2,
+      title: 'Boost your conversion rate',
+      href: '#',
+      description:
+        'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel iusto corrupti dicta laboris incididunt.',
+      date: 'Mar 16, 2020',
+      datetime: '2020-03-16',
+      category: { title: 'Marketing', href: '#' },
+      author: {
+        name: 'Michael Foster',
+        role: 'Co-Founder / CTO',
+        href: '#',
+        imageUrl:
+          'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+      },
+    },
     // More posts...
   ]
   
   export default function Posts() {
     return (
-      <div className="bg-white py-24 sm:py-32">
+      <div className="bg-white py-24 sm:py-10">
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
           <div className="mx-auto max-w-2xl">
-            <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">From the blog</h2>
-            <p className="mt-2 text-lg leading-8 text-gray-600">
-              Learn how to grow your business with our expert advice.
-            </p>
+            <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Posts</h2>
             <div className="mt-10 space-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16">
               {posts.map((post) => (
                 <article key={post.id} className="flex max-w-xl flex-col items-start justify-between">
@@ -52,24 +66,19 @@ const posts = [
                         {post.title}
                       </a>
                     </h3>
-                    <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">{post.description}</p>
-                  </div>
-                  <div className="relative mt-8 flex items-center gap-x-4">
-                    <img src={post.author.imageUrl} alt="" className="h-10 w-10 rounded-full bg-gray-50" />
-                    <div className="text-sm leading-6">
-                      <p className="font-semibold text-gray-900">
-                        <a href={post.author.href}>
-                          <span className="absolute inset-0" />
-                          {post.author.name}
-                        </a>
-                      </p>
-                      <p className="text-gray-600">{post.author.role}</p>
+                    <div className='flex justify-between'>
+                      <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">{post.description}</p>
+                      <div className=''>
+                        <img className='w-40' src='/src/assets/coco.jpg' alt="post image" />
+                      </div>
                     </div>
                   </div>
+                  
                 </article>
               ))}
             </div>
           </div>
+        
         </div>
       </div>
     )
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useMutation, useQuery } from '@apollo/client';
 import { GET_USERS } from '../utils/queries';
+import Posts from '../components/Posts';
 
 const uploadToCloudinary = async (file) => {
     const url = 'https://api.cloudinary.com/v1_1/dk5mamh4v/upload';
@@ -36,16 +37,10 @@ const Profile = () => {
         return <h2>LOADING...</h2>;
     }
 
-    const stats = [
-        { label: 'Followers', value: '44 million' },
-        { label: 'Assets under holding', value: '$119 trillion' },
-        { label: 'New users annually', value: '46,000' },
-    ]
-
 
     return (
         <div className="mx-auto max-w-7xl px-4 sm:px-0 lg:px-8">
-            
+
             {/* We've used 3xl here, but feel free to try other max-widths based on your needs */}
             <div className="container mx-auto sm:px-6 lg:px-8">{/* Content goes here */}
                 <div className='rounded-lg bg-white shadow mt-10'>
@@ -54,11 +49,11 @@ const Profile = () => {
                     </div>
                     <div className=''>
                         <img className='rounded-full w-24 h-24 ml-32 lg:w-40 lg:h-40 lg:-mt-24 -mt-14' src="/src/assets/coco.jpg" alt="" />
-                        <div className='flex pt-5'>
-                            <div className='ml-36 font-bold text-2xl'>zachcygan</div>
-                            <div className='ml-28 font-bold text-2xl'><span className='text-4xl'>22</span>Followers</div>
-                            <div className='ml-28 font-bold text-2xl'><span className='text-4xl'>623</span>Following</div>
-                            <div className='ml-28 font-bold text-2xl'><span className='text-4xl'>54</span>Posts</div>
+                        <div className='flex pt-5 font-bold text-2xl'>
+                            <div className='ml-36'>zachcygan</div>
+                            <div className='ml-28'><span className='text-4xl'>22</span>Followers</div>
+                            <div className='ml-28'><span className='text-4xl'>623</span>Following</div>
+                            <div className='ml-28'><span className='text-4xl'>54</span>Posts</div>
                         </div>
                         <div className='text-lg pt-10 pl-10'>
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam id molestie purus, id consequat tel
@@ -68,13 +63,13 @@ const Profile = () => {
                         </div>
                     </div>
                 </div>
-                <div className='columns-2 mt-10'>
-                    <div className='rounded-lg bg-white shadow'>
-                        Test
-                    </div>
-                    <div className='rounded-lg bg-white shadow'>
-                        Test
-                    </div>
+                
+                <div className='rounded-lg bg-white shadow mt-10'>
+                    <Posts />
+                </div>
+                
+                <div className='rounded-lg bg-white shadow mt-10'>
+                    
                 </div>
             </div>
         </div>
