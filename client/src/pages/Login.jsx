38d7ec4b519e4aceeb844f6ae916d89412f1a662
chunkybooboo <89316044+chunkybooboo@users.noreply.github.com>
2023-06-06 19:02:36 -0700

    added sign up btn

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -73,8 +73,9 @@ const LoginForm = () => {
               </div>
             </div>
       
-            <div>
-              <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
+            <div className='flex justify-center'>
+              <button type="submit" class="flex-col justify-center rounded-md bg-indigo-600 mr-2 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
+              <button type="submit" class="flex-col justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign up</button>
             </div>
           </form>
       
