34e717d0801200ffec7ee2a8c288e44e904fa45d
zachcygan <zachcygan@gmail.com>
2023-06-08 19:38:18 -0700

    commit

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -107,11 +107,6 @@ const Profile = () => {
                                 <button className='m-1 h-8 px-4 text-sm text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg cursor-pointer focus:shadow-outline hover:bg-indigo-800'>
                                     <a href={urlString}> Edit Profile</a>
                                 </button>
-                                {/* <button class="inline-flex items-center h-10 px-5 text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-800">
-                                    <span>With icon</span>
-                                    <svg class="w-4 h-4 ml-3 fill-current" viewBox="0 0 20 20"><path d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clip-rule="evenodd" fill-rule="evenodd"></path></svg>
-                                
-                                </button> */}
                             </div>
                         </div>
                     </div>
