26a60a5f8e8297599da35cf00dfb7da99f116746
zachcygan <zachcygan@gmail.com>
2023-06-09 12:19:07 -0700

    commit

--- client/src/components/Posts.jsx
+++ client/src/components/Posts.jsx
@@ -75,7 +75,17 @@ export default function Posts({ posts }) {
                 </h3>
                 <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">{post.description}</p>
               </div>
-              
+              <div className="relative mt-8 flex items-center gap-x-4">
+                <img src={post.user.profileImage} alt="" className="h-10 w-10 rounded-full bg-gray-100" />
+                <div className="text-sm leading-6">
+                  <p className="font-semibold text-gray-900">
+                    <a href={`/profile/${post.user.username}`}>
+                      <span className="absolute inset-0" />
+                      {post.user.username}
+                    </a>
+                  </p>
+                </div>
+              </div>
             </div>
           </article>
         ))}
