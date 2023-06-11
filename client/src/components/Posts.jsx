8c0e3bc8641a5cfb0a8e62bbaafc830d8ea58050
zachcygan <zachcygan@gmail.com>
2023-06-11 14:32:46 -0700

    added music to post card

--- client/src/components/Posts.jsx
+++ client/src/components/Posts.jsx
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import { LIKE_POST, UNLIKE_POST } from '../utils/mutations';
-import { gql,useMutation, useQuery } from '@apollo/client';
+import { gql, useMutation, useQuery } from '@apollo/client';
 import { GET_ME } from '../utils/queries';
 
-export default function Posts({ posts,handleLike }) {
+export default function Posts({ posts, handleLike }) {
   const { loading, error, data } = useQuery(GET_ME);
   // const [likePost] = useMutation(LIKE_POST,{
   //   refetchQueries: [{ query: GET_ME }],
   // });
-  const [likePost] = useMutation(LIKE_POST,{
+  const [likePost] = useMutation(LIKE_POST, {
     refetchQueries: [{ query: GET_ME }],
     update(cache, { data: { likePost } }) {
       cache.modify({
@@ -37,16 +37,17 @@ export default function Posts({ posts,handleLike }) {
   useEffect(() => {
     if (data) {
       const me = data.me.likedPosts;
-      console.log("liked Posts "+me);
-      const likedPostIds = data.me.likedPosts.map(post=>post._id);
-  
+      console.log(me)
+      console.log("liked Posts " + me);
+      const likedPostIds = data?.me?.likedPosts?.map(post => post._id);
+
       // setusersLikedPosts([...usersLikedPosts, me]);
       setusersLikedPosts(likedPostIds)
       console.log(usersLikedPosts)
-      
+
     }
   }, [data]);
-  
+
 
   function LikeButton({ post }) {
     // const user = getAllLikedPosts();
@@ -59,8 +60,7 @@ export default function Posts({ posts,handleLike }) {
 
 
     const handleLikePost = async (postId) => {
-
-      if(usersLikedPosts.includes(postId)){
+      if (usersLikedPosts.includes(postId)) {
         console.log('User has already liked');
         return;
       }
@@ -102,7 +102,7 @@ export default function Posts({ posts,handleLike }) {
     )
   }
 
-  
+
   return (
     <div className="bg-white py-24 sm:py-32">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
@@ -112,12 +112,7 @@ export default function Posts({ posts,handleLike }) {
               <article className="flex flex-col items-start justify-between p-1">
                 <div className="relative w-full">
 
-                  <div className=' flex row border-8 border-black'>
-                  <img src={post?.selectedMusic?.coverart} alt="music cover" className='w-20 h-30'></img>
-                      <h2>{post?.selectedMusic?.artist}</h2>
-                      <h2>{post?.selectedMusic?.title}</h2>
-                      
-                  </div>
+                  
                   <img
                     src={post.images}
                     alt=""
@@ -126,16 +121,25 @@ export default function Posts({ posts,handleLike }) {
                   <div className="absolute inset-0 rounded-2xl ring-1 ring-inset ring-gray-900/10" />
                 </div>
                 <div className="max-w-xl">
-                <div className='likes-comments flex '>
-                  <div className='flex'>{post?.likes?.length}<svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" className="bi bi-suit-heart like mr-4 ml-2" viewBox="0 0 16 16">
-                    <path d="m8 6.236-.894-1.789c-.222-.443-.607-1.08-1.152-1.595C5.418 2.345 4.776 2 4 2 2.324 2 1 3.326 1 4.92c0 1.211.554 2.066 1.868 3.37.337.334.721.695 1.146 1.093C5.122 10.423 6.5 11.717 8 13.447c1.5-1.73 2.878-3.024 3.986-4.064.425-.398.81-.76 1.146-1.093C14.446 6.986 15 6.131 15 4.92 15 3.326 13.676 2 12 2c-.777 0-1.418.345-1.954.852-.545.515-.93 1.152-1.152 1.595L8 6.236zm.392 8.292a.513.513 0 0 1-.784 0c-1.601-1.902-3.05-3.262-4.243-4.381C1.3 8.208 0 6.989 0 4.92 0 2.755 1.79 1 4 1c1.6 0 2.719 1.05 3.404 2.008.26.365.458.716.596.992a7.55 7.55 0 0 1 .596-.992C9.281 2.049 10.4 1 12 1c2.21 0 4 1.755 4 3.92 0 2.069-1.3 3.288-3.365 5.227-1.193 1.12-2.642 2.48-4.243 4.38z" />
-                  </svg></div>
-                  <div className='flex'>{post?.comments?.length}<svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" className="bi bi-chat-dots comment ml-2" viewBox="0 0 16 16">
-                    <path d="M5 8a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm3 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" />
-                    <path d="m2.165 15.803.02-.004c1.83-.363 2.948-.842 3.468-1.105A9.06 9.06 0 0 0 8 15c4.418 0 8-3.134 8-7s-3.582-7-8-7-8 3.134-8 7c0 1.76.743 3.37 1.97 4.6a10.437 10.437 0 0 1-.524 2.318l-.003.011a10.722 10.722 0 0 1-.244.637c-.079.186.074.394.273.362a21.673 21.673 0 0 0 .693-.125zm.8-3.108a1 1 0 0 0-.287-.801C1.618 10.83 1 9.468 1 8c0-3.192 3.004-6 7-6s7 2.808 7 6c0 3.193-3.004 6-7 6a8.06 8.06 0 0 1-2.088-.272 1 1 0 0 0-.711.074c-.387.196-1.24.57-2.634.893a10.97 10.97 0 0 0 .398-2z" />
-                  </svg></div>
-
-                </div>
+                  
+                  <div className='flex p-1'>
+                      <img src={post?.selectedMusic?.coverart} alt="music cover" className='w-20 h-30'></img>
+                      <div className='align-middle '>
+                        <h2 className='p-1 font-bold'>{post?.selectedMusic?.artist}</h2>
+                        <h2 className='p-1 font-bold'>{post?.selectedMusic?.title}</h2>
+                      </div>
+                  </div>
+                  <div className='flex justify-between'>
+                    <div className='likes-comments flex '>
+                      <div className='flex'>{post?.likes?.length}<svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" className="bi bi-suit-heart like mr-4 ml-2" viewBox="0 0 16 16">
+                        <path d="m8 6.236-.894-1.789c-.222-.443-.607-1.08-1.152-1.595C5.418 2.345 4.776 2 4 2 2.324 2 1 3.326 1 4.92c0 1.211.554 2.066 1.868 3.37.337.334.721.695 1.146 1.093C5.122 10.423 6.5 11.717 8 13.447c1.5-1.73 2.878-3.024 3.986-4.064.425-.398.81-.76 1.146-1.093C14.446 6.986 15 6.131 15 4.92 15 3.326 13.676 2 12 2c-.777 0-1.418.345-1.954.852-.545.515-.93 1.152-1.152 1.595L8 6.236zm.392 8.292a.513.513 0 0 1-.784 0c-1.601-1.902-3.05-3.262-4.243-4.381C1.3 8.208 0 6.989 0 4.92 0 2.755 1.79 1 4 1c1.6 0 2.719 1.05 3.404 2.008.26.365.458.716.596.992a7.55 7.55 0 0 1 .596-.992C9.281 2.049 10.4 1 12 1c2.21 0 4 1.755 4 3.92 0 2.069-1.3 3.288-3.365 5.227-1.193 1.12-2.642 2.48-4.243 4.38z" />
+                      </svg></div>
+                      <div className='flex'>{post?.comments?.length}<svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" className="bi bi-chat-dots comment ml-2" viewBox="0 0 16 16">
+                        <path d="M5 8a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm3 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" />
+                        <path d="m2.165 15.803.02-.004c1.83-.363 2.948-.842 3.468-1.105A9.06 9.06 0 0 0 8 15c4.418 0 8-3.134 8-7s-3.582-7-8-7-8 3.134-8 7c0 1.76.743 3.37 1.97 4.6a10.437 10.437 0 0 1-.524 2.318l-.003.011a10.722 10.722 0 0 1-.244.637c-.079.186.074.394.273.362a21.673 21.673 0 0 0 .693-.125zm.8-3.108a1 1 0 0 0-.287-.801C1.618 10.83 1 9.468 1 8c0-3.192 3.004-6 7-6s7 2.808 7 6c0 3.193-3.004 6-7 6a8.06 8.06 0 0 1-2.088-.272 1 1 0 0 0-.711.074c-.387.196-1.24.57-2.634.893a10.97 10.97 0 0 0 .398-2z" />
+                      </svg></div>
+                    </div>
+                  </div>
                   <div className="group relative">
                     <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
                       <a >
