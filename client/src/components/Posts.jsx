ea16ad799ba72bcb6ed47ebbed360a33032a348c
zachcygan <zachcygan@gmail.com>
2023-06-11 15:25:30 -0700

    fixed like and comment icons with flex

--- client/src/components/Posts.jsx
+++ client/src/components/Posts.jsx
@@ -109,7 +109,7 @@ export default function Posts({ posts, handleLike }) {
         <div className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
           {posts.map((post) => (
             <div key={post._id} className='shadow rounded-lg'>
-              <article className="flex flex-col items-start justify-between p-1">
+              <article className="flex flex-col justify-between p-1">
                 <div className="relative w-full">
                   {post.images ?
                     <img
@@ -137,7 +137,7 @@ export default function Posts({ posts, handleLike }) {
                     </h3>
                     <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">{post.description}</p>
                   </div>
-                  <div className='flex justify-between'>
+                  <div className='flex flex-row justify-between'>
                     <div className="relative mt-8 flex items-center gap-x-4">
                       <img src={post.user.profileImage} alt="" className="h-10 w-10 rounded-full bg-gray-100" />
                       <div className="text-sm leading-6">
@@ -149,7 +149,7 @@ export default function Posts({ posts, handleLike }) {
                         </p>
                       </div>
                     </div>
-                    <div className='mt-10 ml-14 lg:ml-36'>
+                    <div className='mt-10 flex flex-row'>
                       <div className='likes-comments flex'>
                         <div className='flex'>{post?.likes?.length}<svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" className="bi bi-suit-heart like mr-4 ml-2" viewBox="0 0 16 16">
                           <path d="m8 6.236-.894-1.789c-.222-.443-.607-1.08-1.152-1.595C5.418 2.345 4.776 2 4 2 2.324 2 1 3.326 1 4.92c0 1.211.554 2.066 1.868 3.37.337.334.721.695 1.146 1.093C5.122 10.423 6.5 11.717 8 13.447c1.5-1.73 2.878-3.024 3.986-4.064.425-.398.81-.76 1.146-1.093C14.446 6.986 15 6.131 15 4.92 15 3.326 13.676 2 12 2c-.777 0-1.418.345-1.954.852-.545.515-.93 1.152-1.152 1.595L8 6.236zm.392 8.292a.513.513 0 0 1-.784 0c-1.601-1.902-3.05-3.262-4.243-4.381C1.3 8.208 0 6.989 0 4.92 0 2.755 1.79 1 4 1c1.6 0 2.719 1.05 3.404 2.008.26.365.458.716.596.992a7.55 7.55 0 0 1 .596-.992C9.281 2.049 10.4 1 12 1c2.21 0 4 1.755 4 3.92 0 2.069-1.3 3.288-3.365 5.227-1.193 1.12-2.642 2.48-4.243 4.38z" />
