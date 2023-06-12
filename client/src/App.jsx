b2acba9c0006f6f393657986c1673b9f59662e8d
OppedRawen <yud2372@gmail.com>
2023-06-11 22:59:27 -0700

    deployyy

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,7 +16,7 @@ import { Home, Login, Profile, CommunityPost, CreatePost, SearchPage, Register, 
 
 
 const httpLink = createHttpLink({
-  uri: 'https://urrspace.herokuapp.com/',
+  uri: 'https://urrspace.herokuapp.com/graphql',
 });
 
 const authLink = setContext((_, { headers }) => {
