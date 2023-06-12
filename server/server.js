dd5336a3f80d22d9a29769564cf60db0182a9a5d
OppedRawen <yud2372@gmail.com>
2023-06-12 00:38:24 -0700

    fixx??

--- server/server.js
+++ server/server.js
@@ -19,11 +19,13 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
   // app.use(express.static(path.join(__dirname, '../client/build')));
   app.use(express.static(path.join(__dirname, '../client/dist')));
+
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
+  });
 }
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
+
 
 
 // Create a new instance of an Apollo server with the GraphQL schema
